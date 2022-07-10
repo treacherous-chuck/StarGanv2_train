23add30bb0b03645870fd5293e2e25ba08b98337
Chuck <102660676+treacherous-chuck@users.noreply.github.com>
2022-07-10 15:11:55 +0800

    Add files via upload

---
+++ core/utils.py
@@ -0,0 +1,27 @@
+"""
+StarGAN v2 (Train Version)
+
+This work was modified by JONATHON and CHUCK from USTC
+based on https://github.com/clovaai/stargan-v2 (Official Version)
+"""
+
+import torch.nn as nn
+
+
+def print_network(network, name):
+    num_params = 0
+    for p in network.parameters():
+        num_params += p.numel()
+    # print(network)
+    print("Number of parameters of %s: %i" % (name, num_params))
+
+
+def he_init(module):
+    if isinstance(module, nn.Conv2d):
+        nn.init.kaiming_normal_(module.weight, mode='fan_in', nonlinearity='relu')
+        if module.bias is not None:
+            nn.init.constant_(module.bias, 0)
+    if isinstance(module, nn.Linear):
+        nn.init.kaiming_normal_(module.weight, mode='fan_in', nonlinearity='relu')
+        if module.bias is not None:
+            nn.init.constant_(module.bias, 0)
